0f87ddc74b731ef38528ff9c24f5917d1f718ee5
coreash <ksh2594@gmail.com>
2022-01-08 14:00:17 +0900

    first

---
+++ quiz/urls.py
@@ -0,0 +1,8 @@
+import random
+from django.urls import path, include
+from .views import helloAPI, randomQuiz
+
+urlpatterns = [
+    path("hello/", helloAPI),
+    path("<int:id>/", randomQuiz),
+]
